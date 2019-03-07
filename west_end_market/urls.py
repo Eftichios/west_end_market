63a0c69c3eac5c513cc9bc09f3cee61b4d132271
jackleggat <2314380l@student.gla.ac.uk>
2019-03-07 01:01:53 +0000

    Jack: updated views.py and added to urls.py accordingly.

--- west_end_market/urls.py
+++ west_end_market/urls.py
@@ -3,5 +3,11 @@ from west_end_market import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^add_listing/$', views.add_listing, name='add_listing')
+    url(r'^addlisting/$', views.add_listing, name='add_listing'),
+    #url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    #url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^user/$', views.user_profile, name='user_profile')
+    #url(r'^mylistings/$', views.my_listings, name='my_listings'),
+    #url(r'^listing/$', views.show_listing, name='show_listing')
 ]
--- west_end_market/views.py
+++ west_end_market/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
+from west_end_market.models import Listing
 from west_end_market.models import Category as C, Listing as L, User as U
 from west_end_market.forms import ListingForm
 from django.contrib.auth.decorators import login_required
@@ -10,7 +11,9 @@ from datetime import datetime
 
 
 def index(request):
-    return render(request, 'west_end_market/index.html', {})
+    listings_list = Listing.objects.order_by('-date')[:8]
+    context_dict = {'listings': listings_list}
+    return render(request, 'west_end_market/index.html', context_dict)
 
 
 def add_listing(request):
@@ -30,3 +33,47 @@ def add_listing(request):
         print(form.errors)
 
     return render(request, 'west_end_market/add_listing.html', {'form': form})
+
+
+"""
+def register(request):
+    return render(request, 'west_end_market/register.html', {})
+"""
+
+
+def user_login(request):
+    return render(request,'west_end_market/loginpage.html', {})
+
+
+"""
+@login_required
+def user_logout(request):
+    login(request)
+    return HttpResponseRedirect(reverse('index'))
+"""
+
+
+"""
+@login_required
+def my_listings(request):
+    return render(request, 'west_end_market/mylistings.html', {})
+"""
+
+
+"""
+def show_listing(request, listing_id):
+    context_dict = {}
+    try:
+        listing = Listing.objects.get(listing_id=listing_id)
+        comments = Comment.onjects.filter(listing=listing)
+        context_dict['comments'] = comments
+        context_dict['listing'] = listing
+    except Listing.DoesNotExist:
+        context_dict['listing'] = None
+        context_dict['comments'] = None
+    return render(request,'rango/listing.html', context_dict)
+"""
+
+
+def user_profile(request):
+    return render(request, 'west_end_market/user_profile.html', {})
